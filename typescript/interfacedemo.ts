2de9f88e901ffb685ad7bd95bb7160861f448bfe
Anees Khan <mohammadaanees.01@gmail.com>
2026-02-10 12:05:01 +0530

    interfaces

--- typescript/interfacedemo.ts
+++ typescript/interfacedemo.ts
@@ -1,30 +1,78 @@
 
 // base type OR super type OR parent type
 // derived type OR sub  type OR child type
-
+// generalization and specialization
 interface Human{
-    name:string,
-    age:number,
+   readonly id:string,
+    name:string,   // required
+    age?:number,  // option
     email:string,
     address:string,
     show1():void
 }
 
+
+let developer:Human={
+    id:"LH101",
+    name:"Irfan",
+    email:"irfan@gmail.com",
+    age:23,
+    address:"baghat",
+    show1() {
+        console.log("show1 called")
+    },
+}
+
+
+
+
+
+// reopening of interface
+// interface Human{
+//     runningBussiness():string;
+// }
+
 interface Men extends Human{
     salary:number,
     games:string
-    show2():void
+    show2():void,
+    run():void
 }
 
 interface Women extends Human{
     cooking:string,
     washing:string,
-    show3():void
+    show3():void,
+    run():void
 
 }
 
 interface robot extends Men, Women{
 
+}
+
+let obj:robot={
+    name:"Aqsa",
+    age:20,
+    address:"barzulla",
+    email:"aqsa@gmail.com",
+    cooking:"meat",
+    show1(){
+        console.log("show1 called")
+    },
+    show3() {
+        console.log("show3 called")
+    },
+    washing:"closthes",
+    games:"",
+    salary:50000,
+    show2() {
+        
+    },
+    run(){
+        
+    },
+  
 }
 
 // condition Relationship
